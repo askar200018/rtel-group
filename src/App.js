c8d9217c730af3bcf9e12964b4075e6cb5d2b85e
askar200018 <imangali950@gmail.com>
2022-04-13 00:16:01 +0600

    feature: add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
 import TestComponent from './components/TestComponent';
 import TestPage from './pages/TestPage';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="bg-gradient-to-r bg-gray-900">
+      <Header />
       <Routes>
         <Route path="/" element={<TestPage />} />
         <Route path="about" element={<TestComponent />} />
--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,5 +1,5 @@
 const TestPage = () => {
-  return <h1>Test Page</h1>;
+  return <h1 className="text-white">Test Page</h1>;
 };
 
 export default TestPage;
