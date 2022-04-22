07a9afe96a6c122482de2c46e09b892ae4651b05
askar200018 <imangali950@gmail.com>
2022-04-23 03:13:13 +0600

    feature: add map

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,31 +1,48 @@
 import { Container } from '@mui/material';
-import GoogleMapReact from 'google-map-react';
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl';
 import { HeaderHeight } from '../variables/vairables';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoib3NrYXItYWdhIiwiYSI6ImNsMmF2aDlkdDA3NzIza25yZTB6cGlsY3gifQ.Ma1eN-_aYN5AHkewOnTR5A';
 const Projects = () => {
-  const defaultProps = {
-    center: {
-      lat: 51.166667,
-      lng: 71.433333,
-    },
-    zoom: 5,
-  };
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(66.9237);
+  const [lat, setLat] = useState(48.0196);
+  const [zoom, setZoom] = useState(4);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [lng, lat],
+      zoom: zoom,
+    });
+    const test = 'hellllo from test';
+    new mapboxgl.Marker()
+      .setLngLat([71.44598, 51.1801])
+      .setPopup(
+        new mapboxgl.Popup({ className: 'test' }).setHTML(
+          `
+            <h1>Hello World!</h1>
+            <h2 className="text-red-500 test">${test}</h2>
+          `,
+        ),
+      )
+      .addTo(map.current);
+  }, []);
+
   return (
     <div
       style={{
         paddingTop: `${HeaderHeight}px`,
       }}>
       Projects
-      <Container maxWidth="xl">
+      <Container sx={{ position: 'relative' }}>
         <div style={{ height: '520px', width: '100%' }} className="py-8">
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: 'AIzaSyDqDVk27L0HqRCeV2ekSUZa2WLrNmbTmFI' }}
-            defaultCenter={defaultProps.center}
-            defaultZoom={defaultProps.zoom}>
-            <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
-          </GoogleMapReact>
+          <div ref={mapContainer} className="map-container" style={{ height: '100%' }} />
         </div>
       </Container>
     </div>
